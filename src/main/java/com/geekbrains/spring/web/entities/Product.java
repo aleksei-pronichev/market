506d8ab068aeb19fa05b87161d404f09f17c2454
Aleksei Pronichev <41960856+aleksei-pronichev@users.noreply.github.com>
2022-01-09 20:44:51 +0300

    Init (#1)
    * init base app

---
+++ src/main/java/com/geekbrains/spring/web/entities/Product.java
@@ -0,0 +1,30 @@
+package com.geekbrains.spring.web.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Integer price;
+}
